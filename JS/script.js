bdfaeb59b3af338433a6af420892877716068d74
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2023-11-20 16:13:10 +0100

    Problemi con value per fascia d'eta'

--- JS/script.js
+++ JS/script.js
@@ -11,10 +11,11 @@ buttonGenera.addEventListener("click",
         //CHIEDO all'utente la sua eta'
         const userEta = parseInt(document.getElementById("age").value);
         console.log("Eta' dell'utente ", userEta);
+
         //Calcolo del prezzo totale del viaggio (0.21 € al km)
         const priceKm = 0.21;
         let defaultPrice = priceKm * km;
-        console.log(defaultPrice);
+        console.log("Prezzo di base: ", defaultPrice);
 
         //Definisco i valori "minorenne" ed "over65"
         let minorenne = userEta < 18;
@@ -27,19 +28,14 @@ buttonGenera.addEventListener("click",
         // Condizione
         if (minorenne) { //SE minorenne applico uno sconto del 20%
             sconto = defaultPrice * 0.20;
-            console.log(sconto);
+            console.log("Sconto al 20%: ", sconto);
         } else if (over65) { //ALTRIMENTI SE over 65 applico uno sconto del 40%
             sconto = defaultPrice * 0.40;
-            console.log(sconto);
+            console.log("Sconto al 40%: ", sconto);
         }
+
         risultato = (defaultPrice - sconto).toFixed(2);
-        console.log(risultato);
+        console.log("Questo e' il prezzo netto: ", risultato);
+
     }
 )
-
-
-
-
-//OUTPUT finale in forma umana (ovvero piu' comprensibile numericamente parlando)
-// document.getElementById("final_price").innerHTML = risultato.toFixed(2);
-// parseFloat(risultato.toFixed)(2);
