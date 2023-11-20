6bef5f19845ead5c5935cab45f5b28718a134695
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2023-11-20 17:25:23 +0100

    Fix per la fascia d'eta'

--- JS/script.js
+++ JS/script.js
@@ -3,33 +3,32 @@ const buttonGenera = document.getElementById("genera");
 
 // Definisco la mia funzione per prendere i dati inseriti dall'utente per il button
 buttonGenera.addEventListener("click",
+
     function () {
+
+
         //CHIEDO all'utente il numero di km da percorrere
         const km = parseInt(document.getElementById("km").value);
         console.log("Chilometri da fare: ", km);
 
         //CHIEDO all'utente la sua eta'
-        const userEta = parseInt(document.getElementById("age").value);
-        console.log("Eta' dell'utente ", userEta);
+        const userAgeRange = parseInt(document.getElementById("age").value);
+        console.log("Eta' dell'utente ", userAgeRange);
 
         //Calcolo del prezzo totale del viaggio (0.21 € al km)
         const priceKm = 0.21;
         let defaultPrice = priceKm * km;
         console.log("Prezzo di base: ", defaultPrice);
 
-        //Definisco i valori "minorenne" ed "over65"
-        let minorenne = userEta < 18;
-        let over65 = userEta > 65;
-
         //Definisco la mia variabile sullo sconto e del risultato
         let sconto = 0;
         let risultato;
 
         // Condizione
-        if (minorenne) { //SE minorenne applico uno sconto del 20%
+        if (userAgeRange === 1) { //SE minorenne applico uno sconto del 20%
             sconto = defaultPrice * 0.20;
             console.log("Sconto al 20%: ", sconto);
-        } else if (over65) { //ALTRIMENTI SE over 65 applico uno sconto del 40%
+        } else if (userAgeRange === 3) { //ALTRIMENTI SE over 65 applico uno sconto del 40%
             sconto = defaultPrice * 0.40;
             console.log("Sconto al 40%: ", sconto);
         }
